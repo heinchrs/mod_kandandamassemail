123f9aa2b6b100c0e12da5b2390b5f4e24d3def6
heinchrs <heinchrs@gmail.com>
2020-12-30 15:36:17 +0100

    Download URL is now also updated

--- _CreateArchive.ps1
+++ _CreateArchive.ps1
@@ -21,7 +21,11 @@
 # Name of directory containing the current files. This is the name of the extension
 $sExtensionName = (Get-ChildItem).directory.name[0]
 
+# Relative path name of update XML file which is also stored together with zip archive
 $sUpdateXMLfilePath = ".release\" + $sExtensionName + "_update.xml"
+
+# URL where module installation archive is loacated
+$sDownloadURLPath = "https://github.com/heinchrs/mod_kandandamassemail/.release/"
 
 # -----------------------------------------------------------[Execution]------------------------------------------------------------
 
@@ -50,9 +54,16 @@ Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "a -tzip $
 $xml = New-Object XML
 # Load content of update XML
 $xml.Load($sUpdateXMLfilePath)
+
 # Get element containing the version information
-$element =  $xml.SelectSingleNode("//version")
+$elementVersion =  $xml.SelectSingleNode("//version")
 # Update the version info with version from module manifest file
-$element.InnerText = $sVersionInfo
+$elementVersion.InnerText = $sVersionInfo
+
+# Get element containing the download URL information
+$elementURL =  $xml.SelectSingleNode("//downloadurl")
+# Update the download location info according to version from module manifest file
+$elementURL.InnerText = $sDownloadURLPath + $sExtensionName + "-v" + $sVersionInfo + ".zip"
+
 # Save XML update file
 $xml.Save($sUpdateXMLfilePath)
